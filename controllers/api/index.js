d59b4f9f8ef09c909ffbf2508bfb48878b7a2c33
romantic <ida.shalilian@gmail.com>
2021-06-05 10:51:46 -0500

    fixed typo in api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const songRoutes = require('./songRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/projects', songRoutes);
 
 module.exports = router;
