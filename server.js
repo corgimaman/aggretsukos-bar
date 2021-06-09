078f1a69ff4fcc44000f0bc3ca9890b338323a14
Celeste Moore <celestealexmoore@gmail.com>
2021-06-08 21:31:04 -0500

    start of le handlebar

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const Handlebars = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -28,3 +29,6 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App is Now Listening on Port ${PORT}`));
 });
+
+app.engine(Handlebars, hbs.engine);
+app.set("view engine", Handlebars);
