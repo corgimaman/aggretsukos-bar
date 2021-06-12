8f4b15b5eb2a8cbbd0103cfd2cd270d2ee31cb18
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-06-12 14:04:18 -0500

    added the request and the queue scripts

--- public/js/requests.js
+++ public/js/requests.js
@@ -4,3 +4,30 @@
 
 // radio button
 // insert into queue table
+
+
+const requestFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const radio = document.querySelector('#request').value;
+  
+    if (radio) {
+      const response = await fetch('/api/queueRoutes', {
+        method: 'POST',
+        body: JSON.stringify({ radio }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/queue');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+document.querySelector('.request-form')
+.addEventListener('submit', requestFormHandler);
+  
+  
+  
