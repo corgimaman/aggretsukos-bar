633706aa607e0f193bf81204ddc0f5d7261c3962
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-06-13 20:32:40 -0500

    adding menu route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,9 +4,35 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('homepage',{
+        res.render('homepage', {
             style: 'main.css'
         })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/menu', async (req, res) => {
+    try{
+        res.render('menu')
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            //include: [{model: }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
     } catch (err) {
         res.status(500).json(err);
     }
@@ -46,23 +72,6 @@ router.get('/songs', async (req, res) => {
     }
 });
 
-router.get('/profile', withAuth, async (req, res) => {
-    try {
-        const userData = await User.findByPk(req.session.user_id, {
-            attributes: { exclude: ['password'] },
-            //include: [{model: }],
-        });
-
-        const user = userData.get({ plain: true });
-
-        res.render('profile', {
-            ...user,
-            logged_in: true
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
