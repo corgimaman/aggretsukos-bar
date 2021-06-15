6b363cc95194c03ad2e6ac0a2f5b1968cffb7de8
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-06-14 20:48:18 -0500

    cleaning up console logs in requests.js

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,28 +1,23 @@
-// need to validate if song was requested in past 4 hr
-
+// future development: validate if song was requested in past 4 hr
 
 const requestFormHandler = async (event) => {
-     event.preventDefault();
-     console.log('click heard!')
-  
-    const radio = document.querySelector('input[name=songIds]:checked').value;
-    console.log(radio)
-    if (radio)  {
-        console.log('radio exists, going to fetch')
-        const response = await fetch('/api/queue', {
-        method: 'POST',
-        body: JSON.stringify({radio}),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      console.log("hola")
-      if (response.ok) {
-        console.log("hello response ok")
-        document.location.replace('/queue');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const radio = document.querySelector('input[name=songIds]:checked').value;
+  console.log(radio)
+  if (radio)  {
+      const response = await fetch('/api/queue', {
+      method: 'POST',
+      body: JSON.stringify({radio}),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/queue');
+    } else {
+      alert(response.statusText);
     }
-  };
+  }
+};
 
 document.querySelector('#requestForm')
 .addEventListener('submit', requestFormHandler);
